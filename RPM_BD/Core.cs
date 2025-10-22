20828d376d346d24a808b78f91fc4caaa406d962
uwu <potatocuteuwu@gmail.com>
2025-10-22 11:54:18 +0300

    тест коммит

---
+++ RPM_BD/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPM_BD
+{
+    internal class Core
+    {
+        public static car_repairEntities Context = new car_repairEntities();
+    }
+}
--- RPM_BD/Program.cs
+++ RPM_BD/Program.cs
@@ -10,7 +10,12 @@ namespace RPM_BD
     {
         static void Main(string[] args)
         {
-            // ветка master
+            List<Details> det = Core.Context.Details.ToList();
+            foreach (Details d in det)
+            {
+                Console.WriteLine($"{d.id}\t{d.DetailName}");
+            }
+            Console.WriteLine("\n");
         }
     }
 }
